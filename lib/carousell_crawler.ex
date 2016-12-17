135dc8a3d2393842a2a1dca3991f8e28fbfbc8aa
Ping <seymores@gmail.com>
2016-12-18 00:21:02 +0800

    Added more data fields

--- lib/carousell_crawler.ex
+++ lib/carousell_crawler.ex
@@ -1,92 +1,44 @@
 defmodule CarousellCrawler do
   require Logger
+  alias CarousellCrawler.Parser
 
   def get_twitter(url) do
-    load_body(url) |> twitter
+    Parser.load_body(url) |> Parser.twitter
   end
 
-  def twitter(body) do
-    Logger.info inspect(body)
-
-    %{"card" => "meta[name='twitter:card']",
-      "site" => "meta[name='twitter:site']",
-      "creator" => "meta[name='twitter:creator']",
-      "title" => "meta[name='twitter:title']",
-      "description" => "meta[name='twitter:description']",
-      "image" => "meta[name='twitter:image']",
-      "label1" => "meta[name='twitter:label1']",
-      "data1" => "meta[name='twitter:data1']",
-      "label2" => "meta[name='twitter:label2']",
-      "data2" => "meta[name='twitter:data2']"
-      }
-      |> Stream.map(fn {k, v} -> {k, attribute_content(body, v)} end)
-      |> Enum.into(%{})
-  end
-
-  defp attribute_content(body, target) do
-    Floki.find(body, target) |> Floki.attribute("content") |> List.first
-  end
-
-  def load_body(url) do
-    headers = ["User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"]
-    HTTPotion.get(url, [headers: headers, follow_redirects: true]) |> Map.get(:body) |> Floki.parse
-  end
+  # def load_body(url) do
+  #   headers = ["User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"]
+  #   HTTPotion.get(url, [headers: headers, follow_redirects: true]) |> Map.get(:body) |> Floki.parse
+  # end
 
   def product_data(url) do
-    data = parse_for_script_data(url) |> parse_product_store
-    # basic_info = Map.take data, ["timeCreated", "description", "currency", "price", "sellerUsername"]
+    data = Parser.parse_for_script_data(url) |> Parser.parse_product_store
+    Logger.debug inspect(data)
+    basic_info = Map.take data, ["id", "timeCreated", "lastModified", "description",
+                                  "currency", "price", "sellerUsername", "collectionId",
+                                  "currency", "currencyCode", "currencySymbol", "likesCount"]
+    category_name = get_in(data, ["category", "displayName"])
 
+    image = List.first(data["photos"]) |> Map.get("imageUrl")
+
+    latitude = get_in(data, ["marketplace", "location", "latitude"])
+    longitude = get_in(data, ["marketplace", "location", "longitude"])
+    country = get_in(data, ["marketplace", "country", "name"])
+    country_code  = get_in(data, ["marketplace", "country", "code"])
+
+    # Logger.warn inspect(marketplace)
+
+    # r = Map.put(basic_info, "category", category_name)
+    # Map.put(r, "imageUrl", image)
+    Map.merge(basic_info, %{"category" => category_name, "image" => image,
+                            "longitude" => longitude, "latitude" => latitude,
+                            "country" => country, "country_code" => country_code})
   end
 
-  @doc """
-  Parse Carousell item page using embeded script data used by the page react.js.
-
-  ## Example
-
-      iex> CarousellCrawler.parse_for_script_data "https://carousell.com/p/62538322"
-
-  """
-  def parse_for_script_data(url) do
-    load_body(url)
-    |> parse_script
-    |> parse_context_data
-  end
-
-  defp parse_script(body) do
-    Floki.find(body, "script")
-    |> Enum.at(2)
-    |> elem(2)
-    |> List.first
-  end
-
-  defp parse_context_data(script) do
-    Regex.scan(~r/window.App=(.*);/, script)
-    |> List.first
-    |> List.last
-    |> Poison.decode!
-  end
-
-  @doc """
-  Get the product map data from the parse script data.
-
-   ## Example
-
-    iex> data = CarousellCrawler.parse_for_script_data "https://carousell.com/p/62538322"
-    iex> CarousellCrawler.parse_product_store(data)
-
-  """
-  def parse_product_store(data) do
-    # Logger.debug inspect(data)
-    product_id = get_in(data, ["context", "dispatcher", "stores", "ImmutableProductStore", "productListsMap", "PRODUCT"]) |> List.first
-    # product_id = get_in(data, ["context", "dispatcher", "stores", "ImmutableProductStore", "_state", "product"])
-    # get_in(data, ["context", "dispatcher", "stores", "ProductStore", "_state", "productsMap", "#{product_id}"])
-    # Logger.debug product_id
-    # category_id =
-    # get_in(data, ["context", "dispatcher", "stores", "CollectionStore", "collectionsMap", "7"])
-    product_data = get_in(data, ["context", "dispatcher", "stores", "ImmutableProductStore", "productsMap", "#{product_id}"])
-    category_id = product_data["collectionId"]
-    category_data = get_in(data, ["context", "dispatcher", "stores", "CollectionStore", "collectionsMap", "#{category_id}"])
-    Map.put(product_data, "category", category_data)
+  def parse_home_page(url) do
+    Parser.load_body(url)
+    |> Parser.parse_homepage_script
+    |> Parser.parse_context_data
   end
 
 end
---
+++ lib/parser.ex
@@ -0,0 +1,85 @@
+defmodule CarousellCrawler.Parser do
+  require Logger
+
+  def load_body(url) do
+    headers = ["User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"]
+    HTTPotion.get(url, [headers: headers, follow_redirects: true]) |> Map.get(:body) |> Floki.parse
+  end
+
+  def twitter(body) do
+    Logger.info inspect(body)
+
+    %{"card" => "meta[name='twitter:card']",
+      "site" => "meta[name='twitter:site']",
+      "creator" => "meta[name='twitter:creator']",
+      "title" => "meta[name='twitter:title']",
+      "description" => "meta[name='twitter:description']",
+      "image" => "meta[name='twitter:image']",
+      "label1" => "meta[name='twitter:label1']",
+      "data1" => "meta[name='twitter:data1']",
+      "label2" => "meta[name='twitter:label2']",
+      "data2" => "meta[name='twitter:data2']"
+      }
+      |> Stream.map(fn {k, v} -> {k, attribute_content(body, v)} end)
+      |> Enum.into(%{})
+  end
+
+  @doc """
+  Parse Carousell item page using embeded script data used by the page react.js.
+
+  ## Example
+
+      iex> CarousellCrawler.parse_for_script_data "https://carousell.com/p/62538322"
+
+  """
+  def parse_for_script_data(url) do
+    load_body(url)
+    |> parse_script
+    |> parse_context_data
+  end
+
+  defp attribute_content(body, target) do
+    Floki.find(body, target) |> Floki.attribute("content") |> List.first
+  end
+
+  @doc """
+  Get the product map data from the parse script data.
+
+   ## Example
+
+    iex> data = CarousellCrawler.parse_for_script_data "https://carousell.com/p/62538322"
+    iex> CarousellCrawler.parse_product_store(data)
+
+  """
+  def parse_product_store(data) do
+    product_id = get_in(data, ["context", "dispatcher", "stores", "ImmutableProductStore", "productListsMap", "PRODUCT"]) |> List.first
+    product_data = get_in(data, ["context", "dispatcher", "stores", "ImmutableProductStore", "productsMap", "#{product_id}"])
+    category_id = product_data["collectionId"]
+    category_data = get_in(data, ["context", "dispatcher", "stores", "CollectionStore", "collectionsMap", "#{category_id}"])
+    Logger.info inspect(category_data)
+    category_name = Map.take(category_data, ["id", "displayName", "subcategories"])
+    Map.put(product_data, "category", category_name)
+  end
+
+  defp parse_script(body) do
+    Floki.find(body, "script")
+    |> Enum.at(2)
+    |> elem(2)
+    |> List.first
+  end
+
+  def parse_homepage_script(body) do
+    Floki.find(body, "script")
+    |> Enum.at(1)
+    |> elem(2)
+    |> List.first
+  end
+
+  def parse_context_data(script) do
+    Regex.scan(~r/window.App=(.*);/, script)
+    |> List.first
+    |> List.last
+    |> Poison.decode!
+  end
+
+end
